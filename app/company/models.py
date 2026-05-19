9d482cca0ccf0d0185cc4e5f38ae229bb1fc3490
Rajeshwariba Vaghela <r.vaghela2857@gmail.com>
2026-05-19 18:01:38 +0530

    company model created

--- app/company/models.py
+++ app/company/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Company(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=False)
+    company_name = models.CharField(max_length=200, blank=True, null=True)
+    hr_contact = models.CharField(max_length=200, blank=True, null=True)
+    website = models.CharField(max_length=200, blank=True, null=True)
+    approval_status = models.CharField(max_length=200, default='pending')
--- app/student/models.py
+++ app/student/models.py
@@ -4,6 +4,7 @@ from django.db import models
 # Create your models here.
 class Student(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=False)
-    department = models.CharField(blank=True, null=True)
+    department = models.CharField(max_length=200, blank=True, null=True)
     drive = models.IntegerField(blank=True, null=True)
     resume = models.FileField(blank=True, null=True)
+    blacklisted = models.BooleanField(default=False)
