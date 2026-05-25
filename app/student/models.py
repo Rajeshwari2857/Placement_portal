9a397c9a535a953db61d13ce5895689bbec291df
Rajeshwariba Vaghela <r.vaghela2857@gmail.com>
2026-05-25 19:55:34 +0530

    student profile

--- app/student/models.py
+++ app/student/models.py
@@ -6,6 +6,6 @@ class Student(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=False)
     name = models.CharField(max_length=100, blank=True, null=True)
     department = models.CharField(max_length=200, blank=True, null=True)
-    drive = models.IntegerField(blank=True, null=True)
+    graduation_year = models.IntegerField(blank=True, null=True)
     resume = models.FileField(blank=True, null=True)
     blacklisted = models.BooleanField(default=False)
--- app/student/views.py
+++ app/student/views.py
@@ -1,4 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from student import models
 
+
+def stu_profile(request):
+
+    if request.method == 'POST':
+        name = request.POST['name']
+        department = request.POST['department']
+        graduation_year = request.POST['graduation_year']
+        student = models.Student.objects.get(user=request.user) 
+        #request.user gives the user in session
+        student.name = name
+        student.department = department
+        student.graduation_year = graduation_year
+        student.save()  
+        return redirect('stu_dash')
+    
+    else:
+        return render(request, 'stu_profile.html')
+    
 def stu_dash(request):
     return render(request, 'stu_dash.html')
