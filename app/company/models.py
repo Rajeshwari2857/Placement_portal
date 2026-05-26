59bdbe24f0f96749036752e8d2531be9c364937d
Rajeshwariba Vaghela <r.vaghela2857@gmail.com>
2026-05-26 19:03:17 +0530

    drive created

--- app/company/models.py
+++ app/company/models.py
@@ -8,3 +8,12 @@ class Company(models.Model):
     hr_contact = models.CharField(max_length=200, blank=True, null=True)
     website = models.CharField(max_length=200, blank=True, null=True)
     approval_status = models.CharField(max_length=200, default='Pending')
+
+
+class Drive(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, null=False)
+    job_title = models.CharField(max_length=200, blank=True, null=True)
+    job_description = models.TextField(blank=True, null=True)
+    eligibility_criteria = models.TextField(blank=True, null=True)
+    application_deadline = models.DateField(blank=True, null=True)
+    status = models.CharField(max_length=100, default='Pending')
